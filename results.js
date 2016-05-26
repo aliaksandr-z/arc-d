06bd74672e884c70bff76def822873905bb7e1ea
Quan Yang <quanyang@live.com>
2016-05-26 02:02:42 +0200

    Cleaned up code, show only reflected requests and params and click to show response

--- results.js
+++ results.js
@@ -6,8 +6,7 @@ window.addEventListener("load", function() {
 });
 
 window.addEventListener("unload", function() {
-  chrome.debugger.detach({tabId:tabId});
-  window.close();
+
 });
 
 var requests = {};
@@ -20,29 +19,56 @@ function onEvent(debuggeeId, message, params) {
     if (!requests[params.requestId]) {
       requests[params.requestId] = params.request;
     }
-    if (params.redirectResponse)
+    if (params.redirectResponse){
       appendResponse(params.requestId, params.redirectResponse);
+    }
   } else if (message == "Network.responseReceived") {
     appendResponse(params.requestId, params.response);
   }
 }
 
-function appendResponse(requestId, response, method) {
+function appendResponse(requestId, response) {
   chrome.debugger.sendCommand({
     tabId: tabId
   }, "Network.getResponseBody", {
     "requestId": requestId
   }, function(responseData) {
-    responseBody = responseData.body;
-    var request = requests[requestId];
-    if (request.url.toLowerCase().startsWith("data:")){
+    if (chrome.runtime.lastError) {
+    } else {
+      handleResponse(responseData, response, requestId);  
+    }
+  });
+}
+
+function handleResponse(responseData, response, requestId) {
+   if (!responseData) {
       return;
     }
+    responseBody = responseData.body;
+
+    var request = requests[requestId];
+    var requestUrl = request.url;
+    if (requestUrl.toLowerCase().startsWith("data:") || requestUrl.toLowerCase().startsWith("chrome-extension:") || requestUrl.toLowerCase().startsWith("javascript:")) {
+      return;
+    }
+
     var requestRow = document.createElement("tr");
     requestRow.className = "request";
 
+    var timeTd = document.createElement("td");
+    var dt = new Date();
+    var utcDate = dt.toUTCString();
+    timeTd.textContent = utcDate;
+    requestRow.appendChild(timeTd);
+
     var urlTd = document.createElement("td");
-    urlTd.textContent = request.url;
+    urlTd.textContent = requestUrl;
+    urlTd.className = "urlTd";
+    var responseDiv = document.createElement("div");
+    responseDiv.textContent = responseBody;
+    responseDiv.hidden = true;
+    $(urlTd).click(function() {$(responseDiv).toggle()});
+    urlTd.appendChild(responseDiv);
     requestRow.appendChild(urlTd);
 
     var methodTd = document.createElement("td");
@@ -51,24 +77,25 @@ function appendResponse(requestId, response, method) {
 
     var paramsTd = document.createElement("td");
     paramsTd.className = "params";
-    var params = getQueryVariable(request.url);
+    var params = getQueryVariable(requestUrl);
     var reflected = false;
     params.forEach(function(param) {
       var key = unescape(param[0]);
       var value = unescape(param[1]);
-      paramsTd.textContent += (key?key:"") + " = " + (value?value:"");
-      if ((key && responseBody.indexOf(key) > 0) || (value && responseBody.indexOf(value) > 0)) {
+      var detectReflectionFromKeyOrValue = (key && responseBody.indexOf(key) > -1) || (value && responseBody.indexOf(value) > -1);
+
+      if (detectReflectionFromKeyOrValue) {
+        paramsTd.textContent += (key?key:"") + " = " + (value?value:"");
         paramsTd.textContent += " (Reflects)";
+        paramsTd.textContent += "\n";
         reflected = true;
       }
-      paramsTd.textContent += "\n";
     });
-
     requestRow.appendChild(paramsTd);
+
     if (reflected) {
       document.getElementById("result").appendChild(requestRow);
     }
-  });
 }
 
 function formatHeaders(headers) {
