468f8f2fe0c3af2138921c6b5c1caec770ecfb91
Quan Yang <quanyang@live.com>
2016-05-29 02:44:06 +0200

    update debugger to version 1.1

--- background.js
+++ background.js
@@ -1,13 +1,9 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+var version = "1.1";
 
 chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.debugger.attach({tabId:tab.id}, version,
       onAttach.bind(null, tab.id));
 });
-
-var version = "1.0";
 
 function onAttach(tabId) {
   if (chrome.runtime.lastError) {
